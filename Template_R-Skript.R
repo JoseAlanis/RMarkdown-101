d79da67c7bae4f51109d8678fa069f0393e6a9c9
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2023-06-07 00:57:54 +0200

    add first version

---
+++ Template_R-Skript.R
@@ -0,0 +1,10 @@
+# Titel:  R Markdown und reproduzierbarer code - Template R Skript
+# Autor:  Jose C. Garcia Alanis
+# Datum:  07. Juni 20123
+#         Johannes Gutenberg-Universitaet Mainz
+
+# Nutzen Sie dieses Beispiel R-Skript, 
+# um das Kompilieren einer R Markdown-Datei zu üben,
+# mit den zur Verfügung gestellten Tutorial-Materialien unter: 
+
+# place holders
